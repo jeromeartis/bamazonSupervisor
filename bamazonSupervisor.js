a57d65a892a935e1c24c7d2e19bebf3110627c99
Your Name <you@example.com>
2019-04-30 14:30:50 -0400

    fixing script

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,7 +1,9 @@
 let mysql = require('mysql')
 let inquire = require('inquirer')
-require
-var connection = mysql.createConnection({
+
+
+
+let connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -10,162 +12,95 @@ var connection = mysql.createConnection({
 })
 
 function start() {
-    function viewProducts() {
+  console.log('----------------')
+  console.log('Bamazon Supervisor')
+  console.log('----------------\n')
+  inquire
+  .prompt([{
+      type: 'list',
+      message: 'Select from list below what action you would like to complete.',
+      choices: ['Create New Department','View Product Sales by Department'],
+      name: "action"
+  }
+]).then(function (response) {
+      switch (response.action) {
+          case 'View Product Sales by Department':
+              viewProductsBYDept();
+              break;
 
-        connection.connect();
-        connection.query('SELECT * FROM products', function (err, res) {
-            console.log('Current Inventory: ' + '\n')
-            for (i = 0; i < res.length; i++) {
+          case 'Create New Department':
+              createNewDepartment();
+              break;
+      }
+  }).catch(function (error) {
+      throw error;
+  });
+  connection.query('SELECT department_name FROM products', function (err,res){
+    console.table(res)
+  });
+}
+
+function viewProductsBYDept() {
+
+       inquire
+       .prompt([
+         {
+           type: 'input',
+           message: 'Enter Department Name:',
+           name: 'action'
+       }
+     ]).then(function (response) {
+
+        departName = response.action
+         console.log(response.action);
+
+        connection.query(`SELECT * FROM products WHERE department_name = "${departName}"`, function (err, res) {
                 console.table(res)
-            }
+                totalProfit();
+
         })
-    }
+})
+}
 
-    function lowInventory() {
-        connection.connect();
-        connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (error, res) {
-            console.log('Low inventory: ' + '\n')
-            for (j = 0; j < res.length; j++) {
-                console.table(res)
-            }
+function totalProfit (){
+
+  connection.query(`SELECT departments.department_id, departments.department_name, departments.over_head_costs,products.product_sales FROM bamazon.products left join departments on products.item_id = departments.department_id WHERE departments.department_name = "${departName}"`, function (err, res) {
+
+          for (let i = 0; i < res.length; i++){
+            console.log("*******************************")
+            let totalProfit = Number(res[i].over_head_costs)+ Number(res[i].product_sales)
+            // console.table(res[i].over_head_costs);
+            // console.table(res[i].product_sales);
+            console.log(`TOTAL PROFIT: ${totalProfit}`)
+            console.log("*******************************")
+        }
+});
+
+}
+function createNewDepartment() {
+  inquire
+  .prompt([
+    {
+      type: 'input',
+      message:'Enter New Department name',
+      name: 'action'
+    }
+  ]).then(function(response){
+    let input = response.action
+    console.log(input)
+    connection.query("SELECT products.department_name FROM products ", function (error, res) {
+        console.log(`Hellkadjlkaj:${res[0].department_name}`)
+        for (i in res) {
+          for (j in res[i].department_name){
+          if(j === res[i].department_name){
+          console.log("Update")
+        }
+      }
+          }
         })
-    }
-
-    function addInventory() {
-        console.log('-------------------\n' + "Updating Inventory: " + '\n');
-
-        connection.query('SELECT * FROM Products', function (err, res) {
-            if (err) throw err;
-            var itemArray = [];
-            for (var i = 0; i < res.length; i++) {
-                itemArray.push(res[i].product_name);
-            }
-
-            inquire.prompt([{
-                type: "list",
-                name: "product",
-                choices: itemArray,
-                message: "Which item would you like to add inventory?"
-            }, {
-                type: "input",
-                name: "qty",
-                message: "How much would you like to add?",
-                validate: function (value) {
-                    if (isNaN(value) === false) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-            }]).then(function (ans) {
-                var currentQty;
-                for (var i = 0; i < res.length; i++) {
-                    if (res[i].product_name === ans.product) {
-                        currentQty = res[i].stock_quantity;
-                    }
-                }
-                connection.query('UPDATE products SET ? WHERE ?', [{
-                        stock_quantity: currentQty + parseInt(ans.qty)
-                    },
-                    {
-                        product_name: ans.product
-                    }
-                ], function (err, res) {
-                    if (err) throw err;
-                    console.log('The quantity has been successful updated.');
-                    start();
-                });
-            })
-        });
-    }
-    function addNew() {
-        inquire.prompt([
-            {
-                type: "input",
-                name: "newProductName",
-                message: "What product would you like to add?"
-            },
-            {
-                type: "list",
-                name: "department",
-                message: "Which department does this product fall into?",
-                choices: ["Electronics", "Luggage", "Books", "Clothing, Shoes & Jewelry", "Games", "Home Goods", "Automobile"]
-            },
-            {
-                type: "input",
-                name: "cost",
-                message: "How much does it cost?",
-                validate: function(cost) {
-                    if (!isNaN(cost)) {
-                        return true;
-                    }
-                    console.log("Please enter a valid cost.");
-                    return false;
-                }
-            },
-            {
-                type: "input",
-                name: "iniQuantity",
-                message: "How many do we have?",
-                validate: function(iniQuantity) {
-                    if (!isNaN(iniQuantity)) {
-                        return true;
-                    }
-                    console.log("Please enter a valid quantity.");
-                    return false;
-                }
-            }
-        ]).then(function (answers){
-
-            //grab the new product info from answer and add to (insert into) the database table
-            var queryString = "INSERT INTO products SET ?";
-            connection.query(queryString, {
-                product_name: answers.newProductName,
-                department_name: answers.department,
-                price: answers.cost,
-                stock_quantity: answers.iniQuantity,
-            })
-
-            //show message that the product has been added.
-            console.log(answers.newProductName + " has been added to Bamazon.");
+    })
+  }
 
 
-        });
 
-
-    }
-
-        console.log('----------------')
-        console.log('Bamazon Manager')
-        console.log('----------------\n')
-        inquire.prompt([{
-            type: 'list',
-            message: 'Select from list below what action you would like to complete.',
-            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
-            name: "action"
-        }, ]).then(function (answers) {
-            switch (answers.action) {
-                case 'View Products for Sale':
-                    viewProducts();
-                    break;
-
-                case 'View Low Inventory':
-                    lowInventory();
-                    break;
-
-                case 'Add to Inventory':
-                    addInventory();
-                    break;
-
-                case 'Add New Product':
-                    addNew();
-                    break;
-            }
-        }).catch(function (error) {
-            throw error;
-        });
-
-    }
-
-
-    start()
+start();
